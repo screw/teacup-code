f59701cd45acd501576296ad9c7e1de2c488d491
grenville armitage <garmitage@swin.ed.au>
2017-04-06 06:39:35 +1000

    Add support for generating DASH traffic with dash.js inside a browser, from teacup-dash-patch-0.2.tgz

--- experiment.py
+++ experiment.py
@@ -58,7 +58,8 @@ from trafficgens import start_iperf, start_ping, \
     start_httperf_dash, create_http_dash_content, \
     create_http_incast_content, start_httperf_incast, \
     start_nttcp, start_bc_ping, \
-    start_httperf_incast_n, start_fps_game
+    start_httperf_incast_n, start_fps_game, \
+    start_dash_streaming_dashjs
 
 
 ## Collect all the arguments
